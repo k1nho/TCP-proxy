c7a0ea42b5890cd51807ccfce56e79b9abacbd0a
k1nho <kinhong99@gmail.com>
2023-02-03 13:31:39 -0500

    load balancer

---
+++ src/proxy/load_balancer.rs
@@ -0,0 +1,16 @@
+use std::sync::{Arc, Mutex};
+
+/// Round Robin to balance the targets load
+/// **targets** : The vector of targets to select from
+/// **target_id** : A shared mutex guard state per app for each tcp port listener to participate in the round robin
+///
+/// # Return
+/// A String representing the target server
+pub fn round_robin(targets: &Vec<String>, target_id: &Arc<Mutex<usize>>) -> String {
+    let mut id = target_id.lock().unwrap();
+    *id += 1;
+
+    let target_server = targets[*id % targets.len()].clone();
+
+    target_server
+}
